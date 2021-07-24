03f5c15c8ea889aee608ff5bb8c6fa50c2624353
Anton Lozovsky <aloz@ukr.net>
2021-07-24 03:03:13 +0300

    yaml file to create both containers. Just after my Birthday celebration :)

--- ProjectFiles/Docker/2.esl2http/3.run-container.cmd
+++ ProjectFiles/Docker/2.esl2http/3.run-container.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Running esl2http docker container
 
-docker run -it -d --net esl2http --name esl2http --restart on-failure -e esl2http_DBConnectionString="Host=postgres;Username=esl2http;Password=esl2http;Database=esl2http" esl2http
+docker run -it -d --net esl2http --name esl2http --restart on-failure --env-file="../../../esl2http.env" esl2http
 pause 
