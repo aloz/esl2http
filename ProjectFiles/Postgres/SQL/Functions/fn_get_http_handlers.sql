d9db80bc57530d6a610e3a8f888e3517aeb59db7
Anton Lozovsky <aloz@ukr.net>
2021-07-26 01:02:17 +0300

    fn_get_http_handlers: get handlers which are not returned error on post

--- ProjectFiles/Postgres/SQL/Functions/fn_get_http_handlers.sql
+++ ProjectFiles/Postgres/SQL/Functions/fn_get_http_handlers.sql
@@ -1,14 +1,25 @@
-CREATE FUNCTION
+CREATE OR REPLACE FUNCTION
     fn_get_http_handlers()
 RETURNS TABLE (url TEXT)
 LANGUAGE plpgsql
 AS
 $$
 BEGIN
+    
     RETURN query
-    SELECT h.url
+    SELECT
+        h.url
     FROM http_post_handlers h
     WHERE disabled IS NULL
-    ORDER BY h.id;
+    AND id NOT IN (
+        SELECT
+            event_id
+        FROM
+            http_post_statuses
+        WHERE
+            NOT issuccess
+    )
+    ORDER BY id;
+
 END;
-$$ 
+$$
