14ce6540850374adca3890bd1ca3cf512b4107de
Anton Lozovsky <aloz@ukr.net>
2021-07-25 06:09:11 +0300

    http_post_* tables: unique by event and http handler

--- ProjectFiles/Postgres/SQL/Tables/http_post_exceptions.sql
+++ ProjectFiles/Postgres/SQL/Tables/http_post_exceptions.sql
@@ -4,7 +4,8 @@ CREATE TABLE http_post_exceptions (
 	handler_id int4 NOT NULL,
 	posted timestamp NOT NULL DEFAULT now()::timestamp without time zone,
 	exception_text text NULL,
-	CONSTRAINT http_post_exceptions_pk PRIMARY KEY (id)
+	CONSTRAINT http_post_exceptions_pk PRIMARY KEY (id),
+	CONSTRAINT http_post_exceptions_un_event_id_handler_id UNIQUE (event_id, handler_id)
 );
 
 ALTER TABLE http_post_exceptions ADD CONSTRAINT http_post_exceptions_fk_events FOREIGN KEY (event_id) REFERENCES events(id);
--- ProjectFiles/Postgres/SQL/Tables/http_post_statuses.sql
+++ ProjectFiles/Postgres/SQL/Tables/http_post_statuses.sql
@@ -5,11 +5,11 @@ CREATE TABLE http_post_statuses (
 	posted timestamp NOT NULL DEFAULT now()::timestamp without time zone,
 	statuscode int4 NOT NULL,
 	issuccess bool NOT NULL GENERATED ALWAYS AS (statuscode >= 200 AND statuscode < 300) STORED,
-	CONSTRAINT http_post_statuses_pk PRIMARY KEY (id)
+	CONSTRAINT http_post_statuses_pk PRIMARY KEY (id),
+	CONSTRAINT http_post_statuses_un_event_id_handler_id UNIQUE (event_id, handler_id)
 );
 CREATE INDEX http_post_statuses_issuccess_idx ON http_post_statuses USING btree (issuccess);
 CREATE INDEX http_post_statuses_statuscode_idx ON http_post_statuses USING btree (statuscode);
-
 
 ALTER TABLE http_post_statuses ADD CONSTRAINT http_post_statuses_fk_events FOREIGN KEY (event_id) REFERENCES events(id);
 ALTER TABLE http_post_statuses ADD CONSTRAINT http_post_statuses_fk_http_post_handlers FOREIGN KEY (handler_id) REFERENCES http_post_handlers(id);
